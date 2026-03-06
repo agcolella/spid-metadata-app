711490d984c0d6db070c72a36af118dd3e63e9a3
Antonio Colella <colella@agid.gov.it>
2026-03-06 11:46:47 +0100

    fix: rimuovi import bootstrap-italia non utilizzato

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+//import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
